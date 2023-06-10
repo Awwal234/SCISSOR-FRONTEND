53ea65d63047721c6e0e1441ab9a7023abb7dade
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2023-06-10 14:01:03 +0100

    feat: removed firebase hosting :))

--- src/views/app/BitQrcode.vue
+++ src/views/app/BitQrcode.vue
@@ -44,7 +44,6 @@ onMounted(()=>{
             </div>
             <ButtonBlue @click="powerQr" class="bg-[#0E43FF] border border-[#000] text-[#fff]" button-text="Generate QrCode"/>
             <div v-show="qrCode" class="mx-auto w-[75%] h-fit mt-[100px]">
-                <!-- @ts-ignore -->
                 <vue-qrcode class="w-full h-[250px]" :color="{ dark: '#000000ff', light: '#ffffffff' }" :value="qrValue" type="image/png"/>
             </div>
         </div>
