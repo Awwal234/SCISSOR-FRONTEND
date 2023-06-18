4973f7586d1d7050951a14e811b56cae125ef7b7
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2023-06-18 16:16:38 +0100

    feat: fonts to footer

--- src/components/global/FooterView.vue
+++ src/components/global/FooterView.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="mt-[194px]">
         <footer class="w-full py-[20px] px-[27px] h-[270px] bg-[#020E40]">
-            <div class="font-[600] mb-[30px] text-[21px] text-[#fff]">Momo</div>
-            <div class="font-[400] mb-[6px] text-[#fff] text-[18px]">Contact us:</div>
-            <div class="font-[400] text-[#fff] text-[16px]"><a href="mailto:fabowalemuhawwal@gmail.com">fabowalemuhawwal@gmail.com</a></div>
+            <div class="font-[600] font-anton mb-[30px] text-[21px] text-[#fff]">Momo</div>
+            <div class="font-[400] font-anton mb-[6px] text-[#fff] text-[18px]">Contact us:</div>
+            <div class="font-[400] font-inter text-[#fff] text-[16px]"><a href="mailto:fabowalemuhawwal@gmail.com">fabowalemuhawwal@gmail.com</a></div>
         </footer>
     </div>
 </template>
