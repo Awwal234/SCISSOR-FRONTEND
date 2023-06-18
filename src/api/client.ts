ebf71151d6ce2cd312e78f46bf4fe8f64c227c05
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2023-06-18 15:45:53 +0100

    feat: updated frontend

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'https://scissor-backend.onrender.com/api',
     headers: {'content-type': 'application/json'}
 })
 
--- src/components/home/boxingMomo/BoxMomo.vue
+++ src/components/home/boxingMomo/BoxMomo.vue
@@ -17,11 +17,11 @@
             <div class="w-[246px] h-fit bg-[#F3F3F3] px-[24px] py-[20px]">
                 <img src="/img/trendsvg.svg" class="" alt="" loading="lazy"/>
                 <div class="font-anton font-[400] mt-[5px] text-[#000] text-[15px] leading-[20.93px]">
-                    Tracking
+                    Shorten Links
                 </div>
                 <div class="mt-[10px] text-[15px] text-[#000] leading-[18.15px] font-inter font-[200]">
-                    Our website allows you to keep track/records 
-                    of all your links  see more
+                    Our website allows you to keep shorten
+                    all of your long links to shorter ones
                 </div>
             </div>
             <!--end box two-->
@@ -29,11 +29,11 @@
             <div class="w-[246px] h-fit bg-[#F3F3F3] px-[24px] py-[20px]">
                 <img src="/img/trendsvg.svg" class="" alt="" loading="lazy"/>
                 <div class="font-anton font-[400] mt-[5px] text-[#000] text-[15px] leading-[20.93px]">
-                    Tracking
+                    QrCode
                 </div>
                 <div class="mt-[10px] text-[15px] text-[#000] leading-[18.15px] font-inter font-[200]">
-                    Our website allows you to keep track/records 
-                    of all your links  see more
+                    Our website allows you to create qrcodes 
+                    for all your links created  see more
                 </div>
             </div>
             <!--end box three-->
